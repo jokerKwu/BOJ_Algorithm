3215e3b769a8338542bee985d26246bdc9742635
jokerKwu <pkjhj485@naver.com>
2020-05-08 11:58:22 +0900

    Create BOJ_9498.py
    시험 점수

---
+++ Simulation/BOJ_9498.py
@@ -0,0 +1,11 @@
+num = int(input())
+if 90 <= num <= 100:
+    print('A')
+elif 80 <= num < 90:
+    print('B')
+elif 70 <= num < 80:
+    print('C')
+elif 60 <= num < 70:
+    print('D')
+else:
+    print('F')
